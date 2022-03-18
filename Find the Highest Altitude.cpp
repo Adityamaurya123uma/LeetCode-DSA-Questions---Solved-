9548ca7444764c8c7c1bd871430a5adf31fef525
Adityamaurya123uma <63842678+Adityamaurya123uma@users.noreply.github.com>
2022-03-18 23:01:46 +0530

    new commit

---
+++ Find the Highest Altitude.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int n=gain.size();
+        int sum=0;
+        int maxalt=0;
+        
+        for(int i=0;i<n;i++){
+            sum+=gain[i];
+            maxalt=max(maxalt,sum);
+        }
+        return maxalt;
+    }
+};
