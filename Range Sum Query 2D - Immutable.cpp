a550197e9f2ad7707f941bc35ea2c35fd064c944
Adityamaurya123uma <63842678+Adityamaurya123uma@users.noreply.github.com>
2022-03-21 22:12:26 +0530

    new commit

---
+++ Range Sum Query 2D - Immutable.cpp
@@ -0,0 +1,18 @@
+class NumMatrix {
+public:
+     int n,m;
+    vector<vector<int>> sum;
+    NumMatrix(vector<vector<int>>& matrix) {
+        n = matrix.size(), m = matrix[0].size();
+        sum.resize(n + 1, vector<int>(m + 1));
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                sum[i][j] = matrix[i - 1][j - 1] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return sum[row2 + 1][col2 + 1] - sum[row1][col2 + 1] - sum[row2 + 1][col1] + sum[row1][col1];
+    }
+};
