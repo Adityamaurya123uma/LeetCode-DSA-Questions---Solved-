db018a72fc5b6246cf445036526a7b6cfc7de4f4
Adityamaurya123uma <63842678+Adityamaurya123uma@users.noreply.github.com>
2022-04-02 23:43:38 +0530

    new commit

---
+++ Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        set<char>Set;
+        int Max = 0;
+        int start=0;
+        int end=0;
+        
+        while(start<s.size()){
+            auto it = Set.find(s[start]);
+            if(it==Set.end()){
+                if(start-end+1>Max){
+                    Max=start-end+1;
+                }
+                Set.insert(s[start]);
+                start++;
+            }
+            
+            else
+            {
+                Set.erase(s[end]);
+                end++;
+            }
+        }
+        
+        return Max;
+    }
+};
