98a7774de1b8a66a71a7f92fe25d3d18946f84d5
Adityamaurya123uma <63842678+Adityamaurya123uma@users.noreply.github.com>
2022-04-25 22:36:41 +0530

    new commit

---
+++ Peeking Iterator.cpp
@@ -0,0 +1,31 @@
+class PeekingIterator : public Iterator {
+    int nextVal;
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+        nextVal = Iterator::next();
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	    
+	}
+	
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        return nextVal;
+	}
+	
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+        int temp = nextVal;
+        if (Iterator::hasNext())
+            nextVal = Iterator::next();
+        else
+            nextVal = NULL;
+	    return temp;
+	}
+	
+	bool hasNext() const {
+	    return (nextVal != NULL);
+	}
+};
