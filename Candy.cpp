63753e978fed4ad209294a584b56973edfe030d3
Adityamaurya123uma <63842678+Adityamaurya123uma@users.noreply.github.com>
2022-07-04 08:35:31 +0530

    new commit

---
+++ Candy.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+    int candy(vector<int> &ratings)
+    {
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+
+        if (n == 1)
+        {
+            return 1;
+        }
+
+        for (int i = 1; i < n; i++)
+        {
+            if (ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1])
+            {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1])
+            {
+                candies[i] = candies[i + 1] + 1;
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            ans = ans + candies[i];
+        }
+
+        return ans;
+    }
+};
