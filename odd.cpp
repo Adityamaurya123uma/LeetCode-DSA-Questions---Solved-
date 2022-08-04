99d67065dca0dc02f2923f6e163b9232e28360e0
Adityamaurya123uma <63842678+Adityamaurya123uma@users.noreply.github.com>
2022-08-04 22:12:07 +0530

    new commit

--- odd.cpp
+++ odd.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-    int n;
-    cin >> n;
+    int size;
+    cout << "enter the size";
+    cin >> size;
+    int array[size];
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < size; i++)
     {
-
-        for (int j = 1; j < i + 1; j++)
-        {
-            cout << " ";
-        }
-
-        for (int k = 0; k < n - i; k++)
-        {
-            cout << i + 1 << " ";
-        }
-        cout << endl;
+        cin >> array[i];
     }
 
+    for (int i = size - 1; i >= 0; i--)
+    {
+        cout << array[i];
+    }
     return 0;
 }
