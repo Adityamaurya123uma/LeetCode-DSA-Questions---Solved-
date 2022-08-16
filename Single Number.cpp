a30384e88679da94de6a9fbfcca71db08c506041
Adityamaurya123uma <63842678+Adityamaurya123uma@users.noreply.github.com>
2022-08-16 23:18:52 +0530

    new commit

--- Single Number.cpp
+++ Single Number.cpp
@@ -1,11 +1,14 @@
-class Solution {
+class Solution
+{
 public:
-    int singleNumber(vector<int>& nums) {
-        
-        int n=0;
-        
-        for(int i=0;i<nums.size();i++){
-            n^=nums[i];
+    int singleNumber(vector<int> &nums)
+    {
+
+        int n = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            n ^= nums[i];
         }
         return n;
     }
