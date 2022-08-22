68ad3a9a8b1e2f8f6aba879ca397abfef705cf26
Adityamaurya123uma <63842678+Adityamaurya123uma@users.noreply.github.com>
2022-08-22 21:45:51 +0530

    new commit

--- Power of Two.cpp
+++ Power of Two.cpp
@@ -1,11 +1,34 @@
-class Solution {
+class Solution
+{
 public:
-    bool isPowerOfTwo(int n) {
-        if(n==0)
+    bool isPowerOfTwo(int n)
+    {
+        if (n == 0)
             return false;
-        if(n==1)
+        if (n == 1)
             return true;
         else
-            return (n%2==0 && isPowerOfTwo(n/2));
+            return (n % 2 == 0 && isPowerOfTwo(n / 2));
+    }
+};
+
+// Second Approach
+class Solution
+{
+public:
+    bool isPowerOfFour(int n)
+    {
+        if (n == 0)
+            return false;
+
+        while (1)
+        {
+            if (n == 1 || n == 4)
+                return true;
+            if (n % 4 != 0)
+                return false;
+            n = n / 4;
+        };
+        return true;
     }
 };
