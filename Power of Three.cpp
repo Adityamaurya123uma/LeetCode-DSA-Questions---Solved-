9ee81a2ce3b33130439986c776589870231e1402
Adityamaurya123uma <63842678+Adityamaurya123uma@users.noreply.github.com>
2022-08-24 22:10:42 +0530

    new commit

---
+++ Power of Three.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    bool isPowerOfThree(int n)
+    {
+        if (n == 0)
+            return false;
+        if (n == 1)
+            return true;
+        else
+            return (n % 3 == 0 && isPowerOfThree(n / 3));
+    }
+}
