4c20a206b9cd3506575e697b182905c40056a2a7
Adityamaurya123uma <63842678+Adityamaurya123uma@users.noreply.github.com>
2022-09-25 18:19:40 +0530

    new commit

--- test.cpp
+++ test.cpp
@@ -5,4 +5,11 @@ int main()
     string s1;
     string s2;
     cin >> s1, s2;
+
+    int s1l=s1.length();
+    int date;
+
+    for(int i=0;i<s1l;i++){
+        if(s1[i] & s1[i+1] & s1[i+2] & s1[i+3] )
+    }
 }
