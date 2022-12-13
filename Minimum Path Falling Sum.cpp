0b43f498d8d39fe5d99e28744f4945e87acb4107
Adityamaurya123uma <63842678+Adityamaurya123uma@users.noreply.github.com>
2022-12-13 22:21:24 +0530

    New Commit

---
+++ Minimum Path Falling Sum.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+public:
+    int minFallingPathSum(vector<vector<int>> &matrix)
+    {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        // if only row return minimum..
+
+        if (n == 1)
+        {
+            return *min_element(matrix[0].begin(), matrix[0].end());
+        }
+
+        // otherwise traverse the entire matrix and update it
+
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (j == 0)
+                {
+                    matrix[i][j] = matrix[i][j] + min(matrix[i - 1][j], matrix[i - 1][j + 1]);
+                }
+                else if (j == m - 1)
+                {
+                    matrix[i][j] = matrix[i][j] + min(matrix[i - 1][j], matrix[i - 1][j - 1]);
+                }
+                else
+                {
+                    matrix[i][j] = matrix[i][j] + min(matrix[i - 1][j], min(matrix[i - 1][j + 1], matrix[i - 1][j - 1]));
+                }
+            }
+        }
+
+        // return the minimum out of last element
+
+        return *min_element(matrix[n - 1].begin(), matrix[n - 1].end());
+    }
+};
