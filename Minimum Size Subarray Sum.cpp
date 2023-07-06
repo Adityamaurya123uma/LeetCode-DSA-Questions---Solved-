7b58c43daa9f5ecc6c697718353ce4dc005a8e1a
Adityamaurya123uma <63842678+Adityamaurya123uma@users.noreply.github.com>
2023-07-06 21:29:17 +0530

    new commit

--- Minimum Size Subarray Sum.cpp
+++ Minimum Size Subarray Sum.cpp
@@ -3,7 +3,8 @@ public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int n=nums.size();
         int sum=0;
-        int left =0,result=INT_MAX;
+        int left =0;
+        int result=INT_MAX;
         
         for(int i=0;i<n;i++){
             sum = sum+nums[i];
