dc6e5871a5fcc79ec620281e6ef84651983b5adc
Aditya Maurya <63842678+Adityamaurya123uma@users.noreply.github.com>
2023-08-08 22:43:17 +0530

    Code formatting for better readability

--- Search in Rotated Sorted Array.cpp
+++ Search in Rotated Sorted Array.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int low = 0, high = nums.size()-1;
-        
-        while(low<=high) {
-            int mid = low + (high-low)/2;
-            
-            if(nums[mid] == target) return mid;
-            
-            if(nums[high] > nums[mid]) {  // right half
-                if(nums[mid] < target && nums[high] >= target) { // sorted
+    int search ( vector<int>& nums, int target ) {
+        int low = 0;
+        int high = nums.size() - 1;
+        while ( low <= high ) {
+            int mid = low + ( high - low ) / 2;
+            if ( nums[mid] == target ) return mid;           
+            if ( nums[high] > nums[mid] ) {  
+                // right half of the array
+                if ( nums[mid] < target && nums[high] >= target ) { // sorted
                     low = mid + 1;
                 } else {
                     high = mid - 1;
                 }
-            } else { // nums[l] < nums[mid] left half
-                if(nums[low] <= target && nums[mid] > target) { // sorted
+            } else { 
+                // left half of the array
+                if( nums[low] <= target && nums[mid] > target ) { // sorted
                     high = mid -1;
                 } else {
                     low = mid + 1;
