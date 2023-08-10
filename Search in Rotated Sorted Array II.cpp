a335feec188f4431654f25dec572c8f465fc775d
Aditya Maurya <63842678+Adityamaurya123uma@users.noreply.github.com>
2023-08-10 22:28:36 +0530

    Update Search in Rotated Sorted Array II.cpp

--- Search in Rotated Sorted Array II.cpp
+++ Search in Rotated Sorted Array II.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] == target){
-                return true;
+    bool search(vector<int>& arr, int k) {
+        int n = arr.size();
+        int low = 0, high = n-1;
+        while ( low <= high ) {
+            int mid = (low + high) / 2;
+            if ( arr[mid] == k ) return true;
+            if ( arr[low] == arr[mid] && arr[mid] == arr[high] ) {
+                low++, high--;
+                continue;
+            }    
+            if ( arr[low] <= arr[mid] ) {
+                if ( arr[low] <=k && k <= arr[mid] ) {
+                    high = mid - 1;
+                } else {
+                    low = mid+1;
+                }
+            } else {
+                if ( arr[mid] <=k && k <= arr[high] ) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
-        
         return false;
     }
 };
