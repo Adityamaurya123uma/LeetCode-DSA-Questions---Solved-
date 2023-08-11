f757374791b83666fca590a13f57018f77ffbc88
Aditya Maurya <63842678+Adityamaurya123uma@users.noreply.github.com>
2023-08-11 23:02:49 +0530

    Create 518. Coin Change II.cpp

---
+++ 518. Coin Change II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
+            }
+        }
+        
+        return dp[amount];
+    }
+};
