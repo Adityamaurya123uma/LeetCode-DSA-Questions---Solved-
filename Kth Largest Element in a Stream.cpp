93b7ceb52baf732c1ea913faeb71f94b972f36d6
Aditya Maurya <63842678+Adityamaurya123uma@users.noreply.github.com>
2023-08-14 23:30:15 +0530

    Update Kth Largest Element in a Stream.cpp

--- Kth Largest Element in a Stream.cpp
+++ Kth Largest Element in a Stream.cpp
@@ -21,3 +21,25 @@ public:
         return pq.top();
     }
 };
+
+
+////
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        
+       priority_queue<int>pq;
+        
+        for(auto i:nums){
+            pq.push(i);
+        }
+        
+        while(k!=1)
+        {
+            pq.pop();
+            k--;
+        }        
+        return pq.top();
+    }
+};
