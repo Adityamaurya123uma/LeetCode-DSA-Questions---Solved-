8ca5b3519c531677e809ae855758e3931293d3b6
Aditya Maurya <63842678+Adityamaurya123uma@users.noreply.github.com>
2023-08-15 10:18:03 +0530

    Update Find Minimum in Rotated Sorted Array.cpp

--- Find Minimum in Rotated Sorted Array.cpp
+++ Find Minimum in Rotated Sorted Array.cpp
@@ -1,3 +1,5 @@
+// Brute force approach
+// Time complexity : O(n)
 class Solution {
 public:
     int findMin(vector<int>& nums) {
@@ -6,5 +8,32 @@ public:
             minimum = min(minimum,nums[i]);
         }
         return minimum;
+    }
+};
+
+// Optimized approach [Bineary Search]
+// Time complexity : O(logn)
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0;
+        int high = n-1;
+        int ans = INT_MAX;
+        
+        while(low<=high){
+            int mid = (low+high) / 2;
+            if(nums[low] <= nums[mid]){
+                ans = min(ans, nums[low]);
+                low = mid + 1;
+            }
+            if(nums[mid] <= nums[high]){
+                ans = min(ans,nums[mid]);
+                high = mid - 1;
+            }
+        }
+        
+        return ans;
     }
 };
