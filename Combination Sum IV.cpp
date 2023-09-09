c233db021d6dd160b90ce6cf55b863961854c24b
Aditya Maurya <63842678+Adityamaurya123uma@users.noreply.github.com>
2023-09-09 22:42:14 +0530

    Update Combination Sum IV.cpp

--- Combination Sum IV.cpp
+++ Combination Sum IV.cpp
@@ -1,3 +1,15 @@
+Given an array of distinct integers nums and a target integer target, 
+return the number of possible combinations that add up to target.
+The test cases are generated so that the answer can fit in a 32-bit integer.
+
+Example 1:
+Input: nums = [1,2,3], target = 4
+Output: 7
+
+Example 2:
+Input: nums = [9], target = 3
+Output: 0
+
 class Solution
 {
 public:
