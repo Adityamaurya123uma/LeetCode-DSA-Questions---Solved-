37e472f8c844815c1726cd01f6170640bdad13ac
Aditya Maurya <63842678+Adityamaurya123uma@users.noreply.github.com>
2023-09-26 22:31:35 +0530

    Update Remove Duplicate Letters.cpp

--- Remove Duplicate Letters.cpp
+++ Remove Duplicate Letters.cpp
@@ -1,3 +1,14 @@
+Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is 
+the smallest in lexicographical order among all possible results.
+
+Example 1:
+Input: s = "bcabc"
+Output: "abc"
+
+Example 2:
+Input: s = "cbacdcbc"
+Output: "acdb"
+
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
