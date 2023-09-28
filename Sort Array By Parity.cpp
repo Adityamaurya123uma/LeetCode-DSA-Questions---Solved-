4f757fe7e1c42134c5262475b0e2ab8b2e82dffb
Aditya Maurya <63842678+Adityamaurya123uma@users.noreply.github.com>
2023-09-28 22:10:55 +0530

    Update Sort Array By Parity.cpp

--- Sort Array By Parity.cpp
+++ Sort Array By Parity.cpp
@@ -1,52 +1,17 @@
 class Solution {
 public:
-    vector<int> sortArrayByParity(vector<int>& nums) {
-       int n=nums.size();
-        int i=0;
-        int j=n-1;
-        while(i<j){
-            if(nums[i]%2 == 1 && nums[j]%2 == 0)
-                
-            {
-                swap(nums[i],nums[j]);
-                i+=1;
-                j-=1;
-                continue;
-            }
-            if(nums[i]%2 == 0 && nums[j]%2 == 1){
-                i+=1;
-                j-=1;
-                continue;
-            }
-            if(nums[i]%2 == 0 && nums[j]%2 == 0){
-                i+=1;
-                continue;
-                
-            }
-       if(nums[i]%2 == 1 && nums[j]%2 == 1){
-           j-=1;
-           continue;
-        }
+    std::vector<int> sortArrayByParity(std::vector<int>& nums) {
+        int i = 0, j = nums.size() - 1;
+        
+        while (i < j) {
+            while (i < j && nums[i] % 2 == 0)
+                i++;
+            while (i < j && nums[j] % 2 == 1)
+                j--;
+            
+            std::swap(nums[i], nums[j]);
         }
         
         return nums;
     }
 };
-
-// Not optimal - Using extra sapce
-
-//  int n=nums.size();
-//         vector<int> ans;
-        
-//         for(int i=0;i<n;i++){
-//             if(nums[i]%2==0){
-//                 ans.push_back(nums[i]);
-//             }
-//         }
-//         for(int i=0;i<n;i++){
-//             if(nums[i]%2!=0){
-//                 ans.push_back(nums[i]);
-//             }
-//         }
-        
-//         return ans;
